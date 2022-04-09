081af07a3b0b47e91573ae1de380830a6c6ac34a
FernandezDL <fer21747@uvg.edu.gt>
2022-04-08 18:19:56 -0600

    creacion clase leerArchivo y modificaciones en el main

---
+++ src/com/leerArchivo.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+/**
+ * @author Diana
+ *
+ */
+public class leerArchivo {
+
+	public ArrayList<String> leerTxt(String ruta){
+		
+		File archivo= null;
+		FileReader filereader= null;
+		BufferedReader br= null;
+		
+		ArrayList<String> datos= new ArrayList<String>();
+		String filas="";
+		
+		try 
+		{
+			archivo= new File(ruta);
+			filereader= new FileReader(archivo);
+			br= new BufferedReader(filereader);
+			
+			while((filas=br.readLine())!= null)
+			{
+				datos.add(filas);
+			}
+			
+			return datos;
+		}
+		catch(Exception e)
+		{
+			System.out.println("La ruta ingresada no es correcta o no se puede leer el archivo");
+			System.out.println("Por favor intente nuevamente");
+		}
+		
+		return null;
+	}
+}
--- src/com/main.java
+++ src/com/main.java
@@ -3,6 +3,10 @@
  */
 package com;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * @author Diana
  *
@@ -15,6 +19,42 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int option=0;
+		
+		String menu="";
+		String menu2="";
+		String ruta="";
+		
+		Scanner in= new Scanner(System.in);
+		
+		boolean exit=false;
+		
+		while(!exit)
+		{
+			System.out.println("\n\nIngrese la ruta del archivo de texto o escribe \"Exit\" para cerrar la aplicacion");
+			ruta= in.nextLine().toLowerCase();
+			
+			if(ruta.equals("exit"))
+			{
+				exit= true;
+			}
+			
+			else
+			{
+				try {
+                    File archivoPrograma = new File(ruta);
+                    in = new Scanner(archivoPrograma);
+                    while (in.hasNextLine()) {
+                        String data = in.nextLine();
+                        
+                        System.out.println("\n\n-------------------------------------------------------------------------------------------------");
+                        
+                    }
+                } catch (FileNotFoundException ex) {
+                    System.out.println("No se pudo leer el archivo");
+                }
+				
+			}
+		}
 	}
-
 }
